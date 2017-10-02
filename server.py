7ab5c08ebb7a30f15ad542be0d9c058e4e8b0472
Samantha Miller <smiller3@mail.umw.edu>
2017-10-02 13:22:59 +0000

    Added initial files for web app.

---
+++ server.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask, render_template
+app = Flask(__name__)
+
+#Root mapping
+@app.route('/')
+def mainIndex():
+	return render_template('index.html')
+	
+#Displays the Import page to import a document
+@app.route('/import')
+def importPage():
+	return render_template('import.html')
+
+#Displays the Invoice page to create and displays invoices	
+@app.route('/invoice')
+def invoicePage():
+	return render_template('invoice.html')
+
+#Displays a Products page to search for the products the company offers.
+@app.route('/products')
+def productsPage():
+	return render_template('products.html')
+
+
+# start the server
+if __name__ == '__main__':
+    app.run(host=os.getenv('IP', '0.0.0.0'), port =int(os.getenv('PORT', 8080)), debug=True)
