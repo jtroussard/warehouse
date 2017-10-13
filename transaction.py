ba59ecb7bdf7ee5cfa874b3438b6bf637779a050
Taylor Dohmen <tdohmen@mail.umw.edu>
2017-10-12 21:01:20 -0400

    code cleanup

--- transaction.py
+++ transaction.py
@@ -16,40 +16,35 @@ def quantity(warehouseId, productId, cache, db):
 	cursor = db.cursor()
 	if (warehouseId, productId) in cache:
 		return cache[(warehouseId, productId)]
+
 	query = cursor.mogrify(QUANTITY, (productId, warehouseId))
 	cursor.execute(query)
+
 	temp = cursor.fetchone()
 	return temp[0] if temp != None else 0
 
 def newProduct(db, data):
-	cursor = db.cursor()
-	toId = data[0]
-	prodName = data[1]
-	prodDesc = data[2]
-	quantity = data[3]
-	price = data[4]
-	prodNum = data[5]
-	
+	cusor = db.cursor()
+	toId, prodName, prodDesc, quantity, price, prodNum = data
+
 	query = cursor.mogrify(PRODUCTINSERT, (prodName, prodDesc, price, prodNum))
 	cursor.execute(query)
+
 	prodId = cursor.fetchone()
+
 	query = cursor.mogrify(INVENTORYINSERT, (prodId, toId, quantity))
 	cursor.execute(query)
 
 def restock(db, data):
 	cursor = db.cursor()
-	toId = data[0]
-	product = data[1]
-	quantity = data[2]
+	toId, product, quantity = data
+
 	query = cursor.mogrify(INVENTORYUPDATE, (product, toId, quantity))
 	cursor.execute(query)
 
 def transfer(db, data):
 	cursor = db.cursor()
-	fromId = data[0]
-	toId = data[1]
-	product = data[2]
-	transferQuant = data[3]
+	fromId, toId, product, transferQuant = data
 
 	query = cursor.mogrify(QUANTITY, (product, fromId))
 	cursor.execute(query)
@@ -71,41 +66,51 @@ def transfer(db, data):
 
 def validate(line, data, lastProduct, lastWarehouse, db, cache):
 	cursor = db.cursor()
+
 	if len(data) == 6:
 		data[:] = [d.strip() for d in data]
 		data[0] = int(data[0])
 		data[3] = int(data[3])
 		data[4] = float(data[4])
+
 		if data[0] > lastWarehouse:
 			raise InvalidID(NOWAREHOUSE.format(data[0], line))
+
 		lastProduct += 1
 		cache[(data[0], lastProduct)] = data[3]
 		return lastProduct
 	elif len(data) == 3:
 		data[:] = [int(d.strip()) for d in data]
+
 		if data[0] > lastWarehouse:
 			raise InvalidID(NOWAREHOUSE.format(data[0], line))
 		if data[1] > lastProduct:
 			raise InvalidID(NOPRODUCT.format(data[1], line))
+		
 		toQuant = quantity(data[0], data[1], cache, db)
 		cache[(data[0], data[1])] = toQuant + data[2]
 	elif len(data) == 4:
 		data[:] = [int(d.strip()) for d in data]
+
 		if data[0] > lastWarehouse:
 			raise InvalidID(NOWAREHOUSE.format(data[0], line))
 		if data[1] > lastWarehouse:
 			raise InvalidID(NOWAREHOUSE.format(data[1], line))
 		if data[2] > lastProduct:
 			raise InvalidID(NOPRODUCT.format(data[2], line))
+
 		fromQuant = quantity(data[0], data[2], cache, db)
 		if data[3] > fromQuant:
 			raise InventoryOverdraft('Transfering more than warehouse has in stock: Line {0}'.format(line))
+
 		toQuant = quantity(data[1], data[2], cache, db)
 		cache[(data[0], data[2])] = fromQuant - data[3]
 		cache[(data[1], data[2])] = toQuant + data[3]
 	else:
 		raise InvalidOperation('Invalid operation: Line {0}'.format(line))
 
+
+# if operation B is dependent on operation A -> A should preceed B in the file
 def processFile(csvName):
 	db = connect()
 	cursor = db.cursor()
@@ -121,9 +126,9 @@ def processFile(csvName):
 
 	csv = open(csvName, 'r').readlines()
 	csv = [(line + 1, csv[line].strip().split(',')) for line in range(len(csv)) if csv[line][0] not in ('#', '\n')]
-	# data = {line: data[line].strip().split(',') for line in data if csv[line][0] not in ('#', '\n')]
 	errors = []
 	cache = {}
+
 	for line, data in csv:
 		try:
 			lp = validate(line, data, lastProduct, lastWarehouse, db, cache)
