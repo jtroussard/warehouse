28b061894b2faf077f484a2066d1b6fa05bd51ce
jb gateway <gateway@tekksparrows.com>
2017-10-23 10:49:59 -0400

    testing RETURNING clause in invoice insert cmds

--- lib/data_posgresql.py
+++ lib/data_posgresql.py
@@ -88,16 +88,11 @@ def countInvoices():
 	results = results[0][0]
 	return results
 	
-# Return list of sellers
+# Return list of sellers - NOT FINISHED
 def getSellers():
 	conn = connectToPostgres()
 	if conn == None:
 		return None
-	query_string = "SELECT COUNT(DISTINCT saleid) FROM sold";
-	results = execute_query(query_string, conn, select=True, args=None)
-	conn.close()
-	results = results[0][0]
-	return results
 	
 # Insert sales record into DB (via approperaite tables)
 def makeSale(invoiceData):
@@ -111,8 +106,8 @@ def makeSale(invoiceData):
 
 	# Insert sale row
 	saleData = [invoiceData[0]['date'], invoiceData[0]['seller'], invoiceData[0]['customer']]
-	query_string = "INSERT INTO sales (datesold, seller, customerid) VALUES (%s, %s, %s);";
-	results = execute_query(query_string, conn, select=False, args=(tuple(saleData)))
+	query_string = "INSERT INTO sales (datesold, seller, customerid) VALUES
+(%s, %s, %s) RETURNING id AS invoiceNumber;"; results = execute_query(query_string, conn, select=False, args=(tuple(saleData)))
 	
 	# Insert sold row
 	soldData = [invoiceData[0]['product'], invoiceData[0]['qty']]
