79624f53eda75131d61aabcd4950a2355df16ad5
Jacques Troussard <jacques.troussard@gmail.com>
2017-11-09 21:16:16 +0000

    part of last commit - cond check for add inv line

--- static/custom/invoice_ui.js
+++ static/custom/invoice_ui.js
@@ -2,9 +2,14 @@
     var limit = 10;
 
     function addLine(divName){
-        // Testing only really
          if (counter == limit)  {
               alert("You have reached the limit of adding " + counter + " lines");
+         }
+         else if (document.getElementById(counter+"_product").value == "") {
+              alert("Complete current line before adding another.\n\nSee Product Number field.");
+         }
+         else if (document.getElementById(counter+"_quantity").value == "") {
+              alert("Complete current line before adding another.\n\nSee Quantity field.");
          }
          else {
              counter += 1;
@@ -13,23 +18,25 @@
              newLine.setAttribute("class", "row");
              newLine.setAttribute("line", counter);
              
-             // Parent LEFT
+             // Parent LEFT (Product)
              var col_0 = document.createElement("div");
              col_0.setAttribute("class", "col-md-9 mb-3");
              
              // Child LEFT
              var prodInput = document.createElement("input");
+             prodInput.id = counter + "_product";
              prodInput.setAttribute("class", "form-control");
              prodInput.setAttribute("name", "products[]");
              prodInput.setAttribute("required", "");
              prodInput.setAttribute("type", "text");
              
-             // Parent RIGHT
+             // Parent RIGHT (Quantity)
              var col_1 = document.createElement("div");
              col_1.setAttribute("class", "col-md-3 mb-3");
              
              // Child RIGHT
              var qntyInput = document.createElement("input");
+             qntyInput.id = counter + "_quantity";
              qntyInput.setAttribute("class", "form-control");
              qntyInput.setAttribute("name", "qtys[]");
              qntyInput.setAttribute("required", "");
